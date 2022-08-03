b91fb361aef90166e3813baeea731c9643e5fc89
Kamil Popardowski <kamil.popardowski@proplogix.com>
2022-08-03 08:56:23 -0400

    Add base entity

---
+++ CoursePlatformSystem.Course.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace CoursePlatformSystem.Course.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; } 
+    }
+}
