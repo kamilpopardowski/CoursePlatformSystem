8dd86b6ae7f34b3e3e2ad67bcd2cf3a015e2f05d
Kamil Popardowski <kamil.popardowski@proplogix.com>
2022-08-03 08:57:09 -0400

    Add course entity

---
+++ CoursePlatformSystem.Course.Domain/Entities/Course.cs
@@ -0,0 +1,30 @@
+﻿using CoursePlatformSystem.Course.Domain.Exceptions;
+
+namespace CoursePlatformSystem.Course.Domain.Entities
+{
+    public class Course : BaseEntity
+    {
+        private Course()
+        {
+
+        }
+
+        private Course(string name, string description, string createdBy)
+        {
+            Name = name;
+            Description = description;
+            CreatedBy = createdBy;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+
+        public static Course New(string name, string description, string createdBy)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new RequiredNameException();
+
+            return new Course(name, description, createdBy);
+        }
+    }
+}
