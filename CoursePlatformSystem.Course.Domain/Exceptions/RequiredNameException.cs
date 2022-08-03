15394ee1cb37670a15df045541157577a8fe9f0c
Kamil Popardowski <kamil.popardowski@proplogix.com>
2022-08-03 08:56:42 -0400

    Add domain exeption that name is required

---
+++ CoursePlatformSystem.Course.Domain/Exceptions/RequiredNameException.cs
@@ -0,0 +1,9 @@
+﻿namespace CoursePlatformSystem.Course.Domain.Exceptions
+{
+    public class RequiredNameException : Exception
+    {
+        public RequiredNameException() : base("Course name is required.")
+        {
+        }
+    }
+}
